169c43b3e8684961ba19bcdd68154c77abb58f77
Rod Falanga <127354181+Rod-at-DOH@users.noreply.github.com>
2023-11-16 13:11:49 -0700

    added code from the ComputerWorrld/InfoWorld article

--- MeasureAPIPerformance/MeasureAPIPerformance/Controllers/AuthorController.cs
+++ MeasureAPIPerformance/MeasureAPIPerformance/Controllers/AuthorController.cs
@@ -1,9 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeasureAPIPerformance.Models;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace MeasureAPIPerformance.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class AuthorController : ControllerBase
     {
+        private readonly List<Author> authors = new List<Author>();
+
+        public AuthorController()
+        {
+            authors.Add(new Author()
+            {
+                Id = 1,
+                FirstName = "Joydip",
+                LastName = "Kanjilal"
+            });
+            authors.Add(new Author()
+            {
+                Id = 2,
+                FirstName = "Paul",
+                LastName = "Smith"
+            });
+        }
+
+        [HttpGet]
+        public IEnumerable<Author> Get()
+        {
+            return authors;
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public Author Get(int id)
+        {
+            return authors.Find(x => x.Id == id);
+        }
     }
 }
